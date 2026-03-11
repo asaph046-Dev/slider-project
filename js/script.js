39ce9f43cab2eb594d920875bde90b4ce788b190
Asaph França <asaph046@gmail.com>
2026-03-10 23:57:57 -0300

    Finalização

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-const item = document.querySelector('.items')
+const item = document.querySelectorAll('.items')
+const wallPaper = document.querySelector('.main-container')
+const slides = document.querySelector('.slide')
+
+item.forEach(items =>{
+    items.addEventListener('click',() => {
+        const mainContainerBackground = items.style.background
+        wallPaper.style.background = mainContainerBackground
+
+        slides.appendChild(items)
+        
+    })
+})
